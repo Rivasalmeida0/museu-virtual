0b599d3dcc916e399c2389fb123718f4dff55bec
MarioCarvalho <Ricasboy2003@gmail.com>
2026-06-25 22:00:35 +0100

    Login pronto

--- museu_virtual_app/lib/main.dart
+++ museu_virtual_app/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'core/theme/app_theme.dart';
+import 'presentation/providers/auth_providers.dart';
 import 'presentation/screens/home_page.dart';
 import 'presentation/screens/login_screen.dart';
 import 'presentation/screens/gestor/painel_gestor_screen.dart';
@@ -23,7 +24,7 @@ class AngoTechMuseuApp extends StatelessWidget {
       title: 'Museu Virtual de Computadores',
       debugShowCheckedModeBanner: false,
       theme: AppTheme.light,
-      home: const HomePage(),
+      home: const AuthGate(),
       onGenerateRoute: (settings) {
         switch (settings.name) {
           case '/login':
@@ -45,5 +46,42 @@ class AngoTechMuseuApp extends StatelessWidget {
         }
       },
     );
+  }
+}
+
+class AuthGate extends ConsumerStatefulWidget {
+  const AuthGate({super.key});
+
+  @override
+  ConsumerState<AuthGate> createState() => _AuthGateState();
+}
+
+class _AuthGateState extends ConsumerState<AuthGate> {
+  @override
+  void initState() {
+    super.initState();
+    Future.microtask(() => ref.read(authProvider.notifier).checkAuth());
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final authState = ref.watch(authProvider);
+
+    if (authState.status == AuthStatus.unknown) {
+      return const Scaffold(
+        body: Center(child: CircularProgressIndicator()),
+      );
+    }
+
+    if (authState.status == AuthStatus.unauthenticated) {
+      return const LoginScreen();
+    }
+
+    final funcao = (authState.utilizador?['funcao'] as String? ?? '').toLowerCase();
+    if (funcao == 'gestor' || funcao == 'admin') {
+      return const PainelGestorScreen();
+    }
+
+    return const HomePage();
   }
 }
